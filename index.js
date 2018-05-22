dfda6a755808a05b8603dc118fc20a4260cd1460
Prelude <2738607+GitLaboratory@users.noreply.github.com>
2018-05-22 18:04:27 +0700

    Correction

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-let mumbleServer = "paulsurrey.de"; // Just the domain and the port, so its "example.org:64738"
-let webRadioStream = "http://wdr-1live-diggi.icecast.wdr.de/wdr/1live/diggi/mp3/128/stream.mp3?ar-distributor=ffa1"; // The MP3 Stream-Link
+var mumble = require('mumble'),
+    fs = require('fs'),
+    request = require('request'),
+    lame = require('lame');
+
+var options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+let mumbleServer = 'paulsurrey.de'; // Just the domain and the port, so its 'example.org:64738'
+let webRadioStream = 'http://wdr-1live-diggi.icecast.wdr.de/wdr/1live/diggi/mp3/128/stream.mp3?ar-distributor=ffa1'; // The MP3 Stream-Link
 let gain = 0.5; // How loud shall it get? Value must between 0 and 1
+let mumbleUser = 'AudioBot' // The default user name.
 
-const mumble = require('mumble');
-const fs = require('fs');
-const request = require('request');
-const lame = require('lame');
+// const unique = Date.now() % 10;
 
-const unique = Date.now() % 10;
-
-mumble.connect(mumbleServer, function(error, connection) {
+mumble.connect(mumbleServer, options, function (error, connection) {
     if(error) { throw new Error(error); }
-
-    connection.authenticate('stream-' + unique);
+    console.log( 'Connected...' );
+    connection.authenticate(mumbleUser);
+    // connection.authenticate('stream-' + unique);
     connection.on('initialized', function() {
-		console.log("MUM: Connection established");
-
-		const decoder = new lame.Decoder();
-		let s;
-		decoder.on("format", function(f) {
-			console.log(f);
-
-			s.pipe(connection.inputStream({
-				channels: f.channels,
-				sampleRate: f.sampleRate,
-				gain: gain
-			}));
-		});
-
-		s = request(webRadioStream).pipe(decoder);
+        console.log('MUM: Connection established');
+        const decoder = new lame.Decoder();
+        let s;
+        decoder.on('format', function(f) {
+            console.log(f);
+            s.pipe(connection.inputStream({
+                channels: f.channels,
+                sampleRate: f.sampleRate,
+                gain: gain
+            }));
+        });
+        s = request(webRadioStream).pipe(decoder);
     });
 });
